1b9dc50db27b25ad9d37068092c0acf66b76f06e
Нурлан Аминов <alimovn125@gmail.com>
2022-05-29 12:37:40 +1000

    Выдает исключение

--- WeatherBot/Program.cs
+++ WeatherBot/Program.cs
@@ -1,12 +1,83 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
 
 namespace WeatherBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string Token { get; set; } = "5588671272:AAHCqtEfpAOGX7IvA7Y6PBm9Ydk3NMhtwQE";
+        private static TelegramBotClient client;
+
+        static string NameCity;
+        static float tempOfCity;
+        static string nameOfCity;
+
+        static string answerOnWether;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            client = new TelegramBotClient(Token) { Timeout = TimeSpan.FromSeconds(10) };
+
+            var me = client.GetMeAsync().Result;
+            Console.WriteLine($"Bot_Id: {me.Id} \nBot_Name: {me.FirstName} ");
+
+            client.OnMessage += Bot_OnMessage;
+            client.StartReceiving();
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+
+            if (message.Type == MessageType.Text)
+            {
+                NameCity = message.Text;
+                Weather(NameCity);
+                Celsius(tempOfCity);
+                await client.SendTextMessageAsync(message.Chat.Id, $"{answerOnWether} \n\nТемпература в {nameOfCity}: {Math.Round(tempOfCity)} °C");
+
+                Console.WriteLine(message.Text);
+            }
+        }
+
+        public static void Weather(string cityName)
+        {
+            try
+            {
+                string url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "5d18a196257406fa3ed24f17c35f9682";
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest?.GetResponse();
+                string response;
+
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+                ResponseWeather weatherResponse = JsonConvert.DeserializeObject<ResponseWeather>(response);
+
+                nameOfCity = weatherResponse.Name;
+                tempOfCity = weatherResponse.Main.Temp - 273;
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Возникло исключение");
+                return;
+            }
+        }
+
+        public static void Celsius(float celsius)
+        {
+            if (celsius <= 10)
+                answerOnWether = "Сегодня холодно одевайся потеплее!";
+            else
+                answerOnWether = "Сегодня очень жарко, так что можешь одеть маечку и шортики)";
         }
     }
 }
---
+++ WeatherBot/ResponseWeather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherBot
+{
+    class ResponseWeather
+    {
+        public TINFO Main { get; set; }
+
+        public string Name { get; set; }
+    }
+
+}
