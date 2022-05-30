4afc645c8dc540c2d9c4d3d88b1b0198757091f6
Нурлан Аминов <alimovn125@gmail.com>
2022-05-30 18:24:53 +1000

    я понял как исправить ошибки исправлю завтра

--- WeatherBot/Program.cs
+++ WeatherBot/Program.cs
@@ -2,15 +2,122 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Args;
+
+var botClient = new TelegramBotClient(token: "5553659992:AAEqG7xOCD04V9SZfa2DX8-sRQuSV-Ciq7s");
+using var cts = new CancellationTokenSource();
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = { }
+
+};
+
+
+
+botClient.StartReceiving(
+    HandleUpdateAsync, 
+    HandleErrorAsync, 
+    receiverOptions, 
+    cancellationToken: cts.Token);
+var me = botClient.GetMeAsync().Result;
+Console.WriteLine($"Bot_Id: {me.Id} \nBot_Name: {me.FirstName} ");
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Type == UpdateType.Message && update?.Message?.Text != null)
+    {
+        await HandleMessage(botClient, update.Message);
+        return;
+
+    }
+    if (update.Type ==UpdateType.CallbackQuery)
+    {
+        await HandleCallbackQuery(botClient, update.CallbackQuery);
+        return;
+
+    }
+
+}
+async Task HandleMessage(ITelegramBotClient botClient, Message message)
+{
+    if (message.Text == "/start")
+    {
+        await botClient.SendTextMessageAsync(message.Chat.Id, text: "Choose commands: /inline | /keyboard");
+    }
+
+    if (message.Text == "/keyboard")
+    {
+        ReplyKeyboardMarkup keyboard = new(new[]
+        {
+
+            new KeyboardButton[] {"Владивосток"},
+        })
+        {
+
+            ResizeKeyboard = true
+
+        };
+        await botClient.SendTextMessageAsync(message.Chat.Id, text: "Choose:", replyMarkup: keyboard);
+        return;
+
+    }
+}
+
+
+async Task HandleCallbackQuery(ITelegramBotClient botClient, CallbackQuery callbackQuery)
+{
+    if (callbackQuery.Data.StartsWith("buy"))
+    {
+        await botClient.SendTextMessageAsync(
+            callbackQuery.Message.Chat.Id,
+            $"Вы хотите купить?"
+        );
+        return;
+    }
+    if (callbackQuery.Data.StartsWith("sell"))
+    {
+        await botClient.SendTextMessageAsync(
+            callbackQuery.Message.Chat.Id,
+            $"Вы хотите продать?"
+        );
+        return;
+    }
+    await botClient.SendTextMessageAsync(
+        callbackQuery.Message.Chat.Id,
+        $"You choose with data: {callbackQuery.Data}"
+        );
+    return;
+}
+
+
+Task HandleErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Ошибка телеграм АПИ:\n{apiRequestException.ErrorCode}\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
 
 namespace WeatherBot
 {
     class Program
     {
-        private static string Token { get; set; } = "5588671272:AAHCqtEfpAOGX7IvA7Y6PBm9Ydk3NMhtwQE";
+        private static string token { get; set; } = "5553659992:AAEqG7xOCD04V9SZfa2DX8-sRQuSV-Ciq7s";
         private static TelegramBotClient client;
 
         static string NameCity;
@@ -21,18 +128,16 @@ namespace WeatherBot
 
         public static void Main(string[] args)
         {
-            client = new TelegramBotClient(Token) { Timeout = TimeSpan.FromSeconds(10) };
+            client = new TelegramBotClient(token) { Timeout = TimeSpan.FromSeconds(10) };
 
             var me = client.GetMeAsync().Result;
             Console.WriteLine($"Bot_Id: {me.Id} \nBot_Name: {me.FirstName} ");
-
-            client.OnMessage += Bot_OnMessage;
+            сlient.OnMessage += Bot_OnMessage;
             client.StartReceiving();
             Console.ReadLine();
             client.StopReceiving();
         }
-
-        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             var message = e.Message;
 
@@ -46,12 +151,11 @@ namespace WeatherBot
                 Console.WriteLine(message.Text);
             }
         }
-
         public static void Weather(string cityName)
         {
             try
             {
-                string url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "5d18a196257406fa3ed24f17c35f9682";
+                string url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&unit=metric&appid=5d18a196257406fa3ed24f17c35f9682";
                 HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
                 HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest?.GetResponse();
                 string response;
@@ -65,13 +169,13 @@ namespace WeatherBot
                 nameOfCity = weatherResponse.Name;
                 tempOfCity = weatherResponse.Main.Temp - 273;
             }
-            catch (WebException)
+            catch (System.Net.WebException)
             {
                 Console.WriteLine("Возникло исключение");
                 return;
             }
-        }
 
+        }
         public static void Celsius(float celsius)
         {
             if (celsius <= 10)
@@ -80,4 +184,10 @@ namespace WeatherBot
                 answerOnWether = "Сегодня очень жарко, так что можешь одеть маечку и шортики)";
         }
     }
+
+
+
 }
+
+
+
